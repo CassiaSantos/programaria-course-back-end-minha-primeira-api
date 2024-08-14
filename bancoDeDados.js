a6b4d697fa401c2e0955b3495a2f9b86cccf8337
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-08-14 11:06:16 -0300

    feat: implementa segurança no banco de daos com dotenv

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,12 +1,15 @@
 //Traz mongoose para dentro do meu proejto.
 const mongoose = require('mongoose')
+//traz dotenv para o projeto e chama a função config() para que seja possível conectar com o banco de dados:
+require('dotenv').config()
 
 async function conectaBancoDeDados() {
     try {
         console.log("A conexão com banco de dados iniciou")
 
         //espera e conecta com o banco de dados. Enquanto espera, JS resolve demais requisições:
-        await mongoose.connect('mongodb+srv://cassiasantos:myfirstapi@cluster-mulhres-in-tech.xc3ppyf.mongodb.net/?retryWrites=true&w=majority&appName=cluster-mulhres-in-tech')
+        //connect é a abstração do mongoose para conectar com o banco de dados:
+        await mongoose.connect(process.env.MONGO_URL) //string de banco de dados será repassada nesta linha pelo dotenv;
 
         console.log("Conexão com banco de dados realizada com sucesso!")
     } catch(erro) {
